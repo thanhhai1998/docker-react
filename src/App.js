5abf97dce5ebb50aa109684276db6b9e5b96413d
thanhhai <haibt@gto-software.com>
2024-06-09 21:33:16 +0700

    remove ports

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Jstt
+          remove 
+    ports:
+      - '80:80'
         </a>
       </header>
     </div>
