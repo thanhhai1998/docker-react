cc9bbcd77ebfd1452855c305e5c88ca906e9efda
thanhhai <haibt@gto-software.com>
2024-06-10 22:23:39 +0700

    update new app,env

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Jstt
+          Learn React Jstt 10/06
           remove 
     ports:
       - '80:80' ???
